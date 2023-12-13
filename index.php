006d54d5d7ab72d3af91b6998e1d6d82a2b346e5
riccardococciolo <73590570+riccardococciolo@users.noreply.github.com>
2023-12-13 15:47:52 +0100

    add: form

--- index.php
+++ index.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="input-group mb-3">
+        <button class="btn btn-outline-secondary" type="button" id="button-addon1">invia</button>
+        <input type="text" class="form-control" placeholder="inserisci la tua email" aria-label="Example text with button addon" aria-describedby="button-addon1">
+    </div>
 </body>
 </html>
